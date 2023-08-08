14c5360d8e8120f45dd254c18963f6f0bc884cbd
Faith Fechser <faithfechser@gmail.com>
2023-08-07 19:11:21 -0600

    add header component file and add base code

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Header = () => {
+  return (
+    <div class='header'>
+        <h1>Fitness in your Schedule</h1>
+    </div>
+  )
+}
+
+export default Header
