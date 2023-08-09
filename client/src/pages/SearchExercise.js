523a4483285327a2cbf145b8321c7cb69d65b506
Faith Fechser <faithfechser@gmail.com>
2023-08-09 15:17:54 -0600

    create search engine for page

--- client/src/pages/SearchExercise.js
+++ client/src/pages/SearchExercise.js
@@ -1,10 +1,29 @@
 //dependencies
-// import react, {useState} from react
+import React, {useState} from 'react';
 
 const SearchExercise = () => {
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useState([]);
+  
+    const searchEx = async () => {
+      try {
+        // const response = await fetch();
+        // const data = await response.json();
+        setResults(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
     return(
+        // Search engine
         <div className="searchEx">
-            <p>Test</p>
+            <input
+            type="text"
+            placeholder="Search Exercises"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            />
+            <button onClick={searchEx}>Search</button>
         </div>
     )
 }
